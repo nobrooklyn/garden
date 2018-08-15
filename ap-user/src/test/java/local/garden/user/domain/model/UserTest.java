138aac73ff1a5acdaab55f4a870171fa34a11fc7
nobunaga <nobrooklyn@gmail.com>
2018-08-15 22:03:10 +0900

    add coverage plugins

--- ap-user/src/test/java/local/garden/user/domain/model/UserTest.java
+++ ap-user/src/test/java/local/garden/user/domain/model/UserTest.java
@@ -10,6 +10,11 @@ public class UserTest {
     @Test
     public void test_age() {
         User u = new User();
+        u.setId("A");
+        u.setFirstName("john");
+        u.setLastName("Dowe");
+        u.setWeight(65);
+        u.setHeight(174);
         u.setBirthday(LocalDate.of(1980, 1, 8));
         Assert.assertThat("test age", u.age(), CoreMatchers.is(38L));
     }
