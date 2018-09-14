3aa5e08c541d92f6b8e9f3b0633e94abd2cd441a
nobunaga <nobrooklyn@gmail.com>
2018-09-14 22:46:06 +0900

    add micrometer

---
+++ ap-micrometer/src/main/java/local/garden/micrometer/ui/IndexController.java
@@ -0,0 +1,14 @@
+package local.garden.micrometer.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "hello");
+        return "index";
+    }
+}
