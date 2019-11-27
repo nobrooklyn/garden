d708ed81c386a5a46929d8052441069ee9226a99
nobunaga <nobrooklyn@gmail.com>
2019-11-27 22:22:31 +0900

    add AbortIncompleteMultipartUpload

--- ap-cdk/src/main/java/com/myorg/ApCdkStack.java
+++ ap-cdk/src/main/java/com/myorg/ApCdkStack.java
@@ -1,9 +1,14 @@
 package com.myorg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.LifecycleRule;
 
 public class ApCdkStack extends Stack {
     public ApCdkStack(final Construct scope, final String id) {
@@ -13,7 +18,9 @@ public class ApCdkStack extends Stack {
     public ApCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        List<LifecycleRule> lifecycleRules = new ArrayList<>();
+        lifecycleRules.add(LifecycleRule.builder().abortIncompleteMultipartUploadAfter(Duration.days(1)).build());
         Bucket.Builder.create(this, "ap-cdk-bucket").bucketName("ap-cdk-bucket-1").publicReadAccess(false)
-                .versioned(true).build();
+                .versioned(true).lifecycleRules(lifecycleRules).build();
     }
 }
