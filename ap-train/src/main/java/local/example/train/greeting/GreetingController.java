06a2792e1091aa5eebcc4ac016b7d2d3a0e9dce1
nobunaga <nobrooklyn@gmail.com>
2020-02-03 21:47:57 +0900

    add application faml resourcd

--- ap-train/src/main/java/local/example/train/greeting/GreetingController.java
+++ ap-train/src/main/java/local/example/train/greeting/GreetingController.java
@@ -1,9 +1,12 @@
 package local.example.train.greeting;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +18,20 @@ public class GreetingController {
     @Autowired
     private GreetingRepository repository;
 
-    @GetMapping("/hello")
-    public String message(Model model) {
-        Greeting greeting = repository.findById("G3").get();
+    @GetMapping("/")
+    public String list(Model model) {
+        List<Greeting> greetings = repository.findAll();
+        log.info("resutl count = {}", greetings.size());
+        model.addAttribute("greetings", greetings);
+        return "greeting/list";
+    }
+
+    @GetMapping("/{id}")
+    public String message(Model model, @PathVariable("id") String id) {
+        log.info("greeting request id {}", id);
+        Greeting greeting = repository.findById(id).get();
         log.info("greeting = {}", greeting);
         model.addAttribute("message", greeting.getMessage());
-        return "greeting/index";
+        return "greeting/show";
     }
 }
